07d95a7773f3535382ebdddc57c99a3fa460a349
Andrew Courter <exosyphon@yahoo.com>
2023-10-19 10:14:54 -0500

    Try different way of google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=G-PMNS5L31T2}`}
+            />
+            <Script id='google-analytics' strategy="afterInteractive">
+                {`
+                     window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'G-PMNS5L31T2');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
