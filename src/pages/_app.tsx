a5864b10968dfa86aa7298b42e89a867e29bc9f0
Andrew Courter <exosyphon@yahoo.com>
2023-10-19 11:38:36 -0500

    Enhance google analytics to create event per page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import * as ga from '../ga';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            ga.pageview(url);
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <>
             <Script
